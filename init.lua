5d4efc15a30061c968880d11cfaf6db492c9aa82
jeremypries <53063583+jeremypries@users.noreply.github.com>
2023-03-19 12:49:30 -0500

    Add files via upload
    Import settings from Mac

---
+++ init.lua
@@ -0,0 +1,10 @@
+return {
+
+  colorscheme = "oxocarbon",
+
+  plugins = {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.colorscheme.catppuccin" }
+    -- ... import any community contributed plugins here
+  }
+}
